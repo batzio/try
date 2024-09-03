adfa84eb4978336910e1d9d57fc7eacf48cf23fa
batzio <aniandata1234@gmail.com>
2024-09-03 18:10:31 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Lalal from './Components/lalal';
+
+function App() {
+  return (
+    <div className="App">
+        <Lalal/>
+    </div>
+  );
+}
+
+export default App;
